0b613354b4fa53fdfb0ef3f6fab90da12ae4dc30
oJaciel <jacielhbarichellO@gmail.com>
2025-05-21 21:46:07 -0300

    NavBar
    Criada a NavBar da aplicação e cadastrada no app

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <Navbar />
+  </div>
 </template>
+
+  <script>
+import Navbar from "./components/Navbar.vue"
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
+  
+
 
 <style>
 #app {
