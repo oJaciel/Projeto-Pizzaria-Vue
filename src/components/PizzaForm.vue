7a8bc53c6893f71711c74c106c6d5ac501e6f2f2
oJaciel <jacielhbarichellO@gmail.com>
2025-05-21 22:27:19 -0300

    Componentes HomeView
    Criados os componentes que irão compor a tela de Home (a serem implementados).

---
+++ src/components/PizzaForm.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>CAMPOS....</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PizzaForm",
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
 <template>
-  <div class='home'>
-    <h1>Home</h1>
+  <div>
+    <Banner />
+    <h1>Monte a sua pizza</h1>
+    <PizzaForm />
   </div>
 </template>
 
 <script>
-
+  import Banner from '../components/Banner.vue'
+  import PizzaForm from '../components/PizzaForm.vue'
+  export default {
+    name: "HomeView",
+    components: {
+      Banner,
+      PizzaForm,
+    }
+  }
 </script>
