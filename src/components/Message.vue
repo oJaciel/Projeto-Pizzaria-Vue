ca960459657e2799df19045fc254d2c3fac62fa6
oJaciel <jacielhbarichellO@gmail.com>
2025-06-11 22:23:49 -0300

    Mensagem no Formulário
    Criada mensagem, que é mostrada ao adicionar um novo pedido através do formulário.

---
+++ src/components/Message.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="message-container">
+        <p> {{ msg }} </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Message",
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
